e2002d00e88ed110990ffc07291b7c713c63156f
OmShanbhag <41207581+OmShanbhag@users.noreply.github.com>
2026-04-03 03:22:12 +0000

    Deploying to gh-pages from @ OmShanbhag/foliosite@531e869b7c5c4d3c98cb8d476efd3a079fc800e2 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://omshanbhag.github.io/foliosite//#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://omshanbhag.github.io/foliosite//foliosite/#contact">contact us</a>.'
               }
             ]
           }
